abaf9d5c97c1d96120b94c755b5bb84bda7b9457
Tyler Mitchell <tylermitchellisawesome@gmail.com>
2020-10-30 10:09:31 -0400

    Update User.js
    fixed user model

--- server/models/User.js
+++ server/models/User.js
@@ -2,14 +2,22 @@ const { DataTypes } = require('sequelize');
 const db = require('../db');
 
 const User = db.define('user', {
-  email: {
+  username: {
     type: DataTypes.STRING(100),
     allowNull: false,
     unique: true
   },
-  password: {
+    passwordhash: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  url_userimage: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  screen_name: {
+    type: DataTypes.STRING,
+    allowNull: true
   }
 
 });
