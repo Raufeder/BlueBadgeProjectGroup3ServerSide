ffc9602ba8b8029efc7a073ff06061b8446ee98f
Sally Mellinger <sallymellinger@MacBook-Pro.local>
2020-10-30 12:56:59 -0400

    friday pm added delete account and delete character

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -74,6 +74,19 @@ userController.post('/login', function(req, res) {
 
 
 // TODO Delete Account Route
+UserController.delete('/delete', function (req, res) {
+  
+    User.destroy({
+        where: { username: req.body.user.username }
+    }).then (
+        function deleteAccountSuccess(data){
+            res.send("Your account has been deleted.")
+        },
+        function deleteLogError(err){
+            res.send(500, err.message);
+        }
+    ); 
+});
 // TODO Change password Route
 
 module.exports = userController;
