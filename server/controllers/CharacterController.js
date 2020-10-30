2bcacb9fad2344d93ba898b0ff896e08cf6e8f52
David Petrie <davidpapasmurf@Davids-MacBook-Pro.local>
2020-10-30 09:13:32 -0400

    login & register route

--- server/controllers/CharacterController.js
+++ server/controllers/CharacterController.js
@@ -6,12 +6,36 @@ const characterController = Router();
 characterController.post('/test', function(req, res){
     res.send("Test went through!")
 });
+
+//TODO Character List Route
+characterController.get("/", async (req, res) => {
+    try {
+        const lists = await Character.findAll({
+            where: {
+                createdBy: req.user.id,
+            },
+        });
+        if (lists) {
+            res.status(200).json({
+                results: lists
+            });
+        } else {
+            res.status(404).json({
+                message: "No lists found for user",
+            });
+        }
+    } catch (e) {
+        res.status(500).message({
+            message: "Failed to retrieve lists for user",
+        });
+    }
+});
+
+
 /*
 TODO Create Character Route
 TODO Edit Character Route
 TODO Delete Character Route
-TODO Character List Route
-TODO 
 */
 
 module.exports = characterController;
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -11,7 +11,67 @@ userController.post('/test', function(req, res){
 });
 
 //TODO Register Route
+userController.post('/register', function(req, res) {
+    let email = req.body.user.email;
+    let password = req.body.user.password;
+
+    User.create({
+        email: email,
+        password: bcrypt.hashSync(password, 12)
+    }).then(
+        function createSuccess(user) {
+            var token = jwt.sign({id: user.id}, process.env.JWT_SECRET,
+                {expiresIn: 60*60*24});
+
+                res.json({
+                    user: user,
+                    message: 'User registered',
+                    sessionToken: token
+                });
+        },
+        function createError(err) {
+            if (err instanceof UniqueConstraintError) {
+                res.status(409).json({
+                    message: 'Email already in use.'
+                });
+            } else {
+                res.status(500).json({
+                    message: 'Failed to register user'
+                });
+            }
+        }
+    );
+});  
+
 // TODO Login Route
+userController.post('/login', function(req, res) {
+    User.findOne( { where: { email: req.body.user.email } } ).then(
+
+        function(user) {
+            if (user) {
+                bcrypt.compare(req.body.user.password, user.password, function (err, matches) {
+                    if (matches) {
+                        var token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
+                        res.json({
+                            user: user,
+                            message: "Successful Login!",
+                            sessionToken: token
+                        });
+                    }else {
+                        res.status(502).send({ error: "you failed haha!"});
+                    }
+                });
+            } else {
+                res.status(500).send({ error: "failed to authenticate"});
+            }
+        },
+    function (err) {
+        res.status(501).send({ error: "you failed, haha!!" });
+    }
+  );
+});
+
+
 // TODO Delete Account Route
 // TODO Change password Route
 
