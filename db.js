3d2bacb986dff793f15d9754fa8b3551c43238b4
Sally Mellinger <sallymellinger@MacBook-Pro.local>
2020-11-04 13:46:06 -0500

    wed pm

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL, {dialect: "postgres"});
+const db = new Sequelize(process.env.DATABASE_URL ||
+    `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/personary`,
+     {dialect: "postgres"});
 
 module.exports = db;
