a2611690974b34e705f86cb25d893cfa6eba140b
Tyler Mitchell <tylermitchellisawesome@gmail.com>
2020-11-03 23:11:44 -0500

    trying manual start on heroku

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DB_CONNECTION_STRING, {dialect: "postgres"});
+const db = new Sequelize(process.env.Db, process.env.DB_USER, process.env.DB_PASS, {dialect: "postgres"});
 
 module.exports = db;
